63032ff377cf827869f4bcadd8df47ffe1fe6e62
Luke Nzang'u <saintlukke@gmail.com>
2019-01-11 04:14:11 +0300

    [Starts #163100204] Write tests for login endpoint.

--- app/tests/v1/test_authentication.py
+++ app/tests/v1/test_authentication.py
@@ -6,7 +6,30 @@ def test_user_register(client):
         'email': 'lukey@app.com',
         'phone_number': '+254726094972',
         'username': 'Kingpin',
-        'password': 'password'
+        'password': 'password',
+        'admin': 'false'
     }
     response = client.post('api/v1/auth/user/register', json=user)
     assert response.status_code == 201
+
+def register_user(client):
+    user = {
+        'firstname': 'Luke',
+        'lastname': 'Nzang\'u',
+        'othername': 'Kanga',
+        'email': 'lukey@app.com',
+        'phone_number': '+254726094972',
+        'username': 'Kingpin',
+        'password': 'password',
+        'admin': 'true'
+    }
+    client.post('api/v1/auth/user/register', json=user)
+
+def test_user_login(client):
+    register_user(client)
+    user = {
+        'email': 'lukey@app.com',
+        'password': 'password'
+    }
+    response = client.post('api/v1/auth/user/login', json=user)
+    assert response.status_code == 200
